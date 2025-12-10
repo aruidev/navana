d20c1e5be443796bf0efee2d8a9183004a9b3dfd
Alex Ruiz <a.ruiz7@sapalomera.cat>
2025-12-10 15:26:02 +0100

    style: refactor header and footer styles and moved to global styles.css file

--- app/view/layout/footer.php
+++ app/view/layout/footer.php
@@ -1,22 +1,5 @@
 <?php
 ?>
-<style>
-    .footer {
-        background-color: transparent;
-        color: gray;
-        text-align: center;
-        position: static;
-        width: 100%;
-        bottom: 0;
-        margin-top: 1.5rem;
-    }
-
-    .inner-footer {
-        max-width: 100%;
-        padding: 0.5rem 0;
-        border-top: 2px solid lightgray;
-    }
-</style>
 <footer class="footer">
     <div class="inner-footer">
         <p>Alex Ruiz - DAW2</p>
